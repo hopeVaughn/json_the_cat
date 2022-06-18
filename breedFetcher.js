0933551095323ab80328b66065233d7b4b8d14e2
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-18 13:40:29 -0700

    move breedFetcher.js functionality into another function and import to index.js. tested, working, linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,30 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
+const fetchBreedDescription = function(breedName, callback) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  if (error !== null || response.statusCode !== 200) {
-    console.error(`error: ${error} \nstatus: ${response.statusCode}`);
-    return;
-  } // Print the error if one occurred
-
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.error(`Your request for ${args} is invalid please enter a valid search parameter`);
-    return;
-  }
-  if (data.length > 1) {
-    for (const ele of data) {
-      console.log(ele.name + ": " + ele.description + '\n');
+    if (error !== null || response.statusCode !== 200) {
+      return callback(error, null);
     }
-  } else {
-    console.log(data[0].description);
-  }
-});
+
+    console.log('statusCode:', response && response.statusCode);
+
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback(`Your request for ${breedName} is invalid please enter a valid search parameter`, null);
+    } else {
+      callback(null, data[0].description);
+    }
+    // if (data.length > 1) {
+    //   for (const ele of data) {
+    //     console.log(ele.name + ": " + ele.description + '\n');
+    //   }
+    // } else {
+    // }
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
